69dc8f1bdf0eb9ef40e4ca10c271c56daa248c12
Leon Balliet <leon.balliet@gmx.de>
2026-05-03 22:39:14 +0200

    Update URL patterns to include new views

--- qngapp/urls.py
+++ qngapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from . import views
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", views.building_view, name="building"),
+    path("scenario/", views.scenario_view, name="scenario"),
 ]

cfc775555c84dbb967fee7cbc2c0dca67e900d08
Leon Balliet <leon.balliet@gmx.de>
2026-05-03 22:57:54 +0200

    Update views.py

--- qngapp/views.py
+++ qngapp/views.py
@@ -34,25 +34,41 @@ def scenario_view(request):
     if not building_data:
         return redirect("building")
 
-    result = None
+    scenario_data = request.session.get("scenario_data", {
+        "heating": "Nahwärme, Pelletkessel",
+        "ventilation": "Zu-/Abluftanlage mit WRG",
+        "pv_area": "300",
+        "battery_storage": "nein",
+        "qng_level": "QNG-PLUS",
+    })
 
     if request.method == "POST":
-        result = calculate_qng_result(
-            nrf_total=building_data["nrf_total"],
-            nrf_tg=building_data["nrf_tg"],
-            nrf_heated=building_data["nrf_heated"],
-            an_geg=building_data["an_geg"],
-            building_type=building_data["building_type"],
-            energy_standard=building_data["energy_standard"],
-            heating=request.POST.get("heating"),
-            ventilation=request.POST.get("ventilation"),
-            qng_level=request.POST.get("qng_level"),
-            pv_area=request.POST.get("pv_area", "0"),
-            battery_storage=request.POST.get("battery_storage", "nein"),
-        )
+        scenario_data = {
+            "heating": request.POST.get("heating"),
+            "ventilation": request.POST.get("ventilation"),
+            "pv_area": request.POST.get("pv_area", "300"),
+            "battery_storage": request.POST.get("battery_storage", "nein"),
+            "qng_level": request.POST.get("qng_level"),
+        }
+        request.session["scenario_data"] = scenario_data
+
+    result = calculate_qng_result(
+        nrf_total=building_data["nrf_total"],
+        nrf_tg=building_data["nrf_tg"],
+        nrf_heated=building_data["nrf_heated"],
+        an_geg=building_data["an_geg"],
+        building_type=building_data["building_type"],
+        energy_standard=building_data["energy_standard"],
+        heating=scenario_data["heating"],
+        ventilation=scenario_data["ventilation"],
+        qng_level=scenario_data["qng_level"],
+        pv_area=scenario_data["pv_area"],
+        battery_storage=scenario_data["battery_storage"],
+    )
 
     return render(request, "qngapp/scenario.html", {
         "building": building_data,
+        "scenario": scenario_data,
         "heating_systems": KG400_GROSSGERAETE_VALUES["heating"].keys(),
         "ventilation_systems": KG400_GROSSGERAETE_VALUES["ventilation"].keys(),
         "qng_levels": QNG_LIMITS.keys(),

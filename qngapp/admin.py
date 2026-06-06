ee1c340a38baeb8ecf578c83e98e6d35aa0a2e95
Leon Balliet <leon.balliet@gmx.de>
2026-06-06 19:40:53 +0200

    Update admin.py

--- qngapp/admin.py
+++ qngapp/admin.py
@@ -1,3 +1,64 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Building, Scenario
+
+
+admin.site.site_header = "QNG Admin"
+admin.site.site_title = "QNG Backend"
+admin.site.index_title = "Projektverwaltung"
+
+
+@admin.register(Building)
+class BuildingAdmin(admin.ModelAdmin):
+    list_display = (
+        "project_name",
+        "building_type",
+        "energy_standard",
+        "nrf_total",
+        "nrf_heated",
+        "an_geg",
+        "created_at",
+    )
+
+    search_fields = (
+        "project_name",
+    )
+
+    list_filter = (
+        "building_type",
+        "energy_standard",
+    )
+
+    ordering = (
+        "-created_at",
+    )
+
+
+@admin.register(Scenario)
+class ScenarioAdmin(admin.ModelAdmin):
+    list_display = (
+        "building",
+        "heating",
+        "ventilation",
+        "qng_level",
+        "pv_area",
+        "battery_storage",
+        "created_at",
+    )
+
+    list_filter = (
+        "qng_level",
+        "battery_storage",
+        "heating",
+        "ventilation",
+    )
+
+    search_fields = (
+        "building__project_name",
+        "heating",
+        "ventilation",
+    )
+
+    ordering = (
+        "-created_at",
+    )

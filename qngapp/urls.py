fa17bac87898cf25b5be4cb268a5c7783a845ba3
Leon Balliet <leon.balliet@gmx.de>
2026-06-06 20:35:59 +0200

    Update urls.py

--- qngapp/urls.py
+++ qngapp/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.building_view, name="building"),
     path("scenario/", views.scenario_view, name="scenario"),
-
     path("projects/", views.project_list_view, name="projects"),
+    path("project/<int:project_id>/", views.project_detail_view, name="project_detail"),
 ]

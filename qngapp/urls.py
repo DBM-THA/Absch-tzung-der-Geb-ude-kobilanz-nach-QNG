9cef1b79f35f37c17ba98ea64628782efb66f910
Leon Balliet <leon.balliet@gmx.de>
2026-06-06 23:11:57 +0200

    Add compare scenarios path to URLs

--- qngapp/urls.py
+++ qngapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("scenario/", views.scenario_view, name="scenario"),
     path("projects/", views.project_list_view, name="projects"),
     path("project/<int:project_id>/", views.project_detail_view, name="project_detail"),
+    path("project/<int:project_id>/compare/", views.compare_scenarios_view, name="compare_scenarios"),
 ]

0a39b5c5531a2942f8569c13cc8392c15b3abbdf
Leon Balliet <leon.balliet@gmx.de>
2026-06-23 01:01:43 +0200

    Add route for adding scenario to project

--- qngapp/urls.py
+++ qngapp/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path("projects/", views.project_list_view, name="projects"),
     path("project/<int:project_id>/", views.project_detail_view, name="project_detail"),
     path("project/<int:project_id>/compare/", views.compare_scenarios_view, name="compare_scenarios"),
+    path("project/<int:project_id>/add-scenario/", views.add_scenario_to_project_view, name="add_scenario_to_project"),
     path("project/<int:project_id>/delete/", views.delete_project_view, name="delete_project"),
     path("scenario/<int:scenario_id>/delete/", views.delete_scenario_view, name="delete_scenario"),
 ]
